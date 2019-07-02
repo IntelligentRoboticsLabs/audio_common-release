25a1527a75ea2abe86afc36a80b5ac66d00b1ffc
jgines <jonathangines@hotmail.com>
2019-07-02 09:15:33 +0200

    Add ros_soundclient to improve time to speak

---
+++ scripts/ros_soundclient.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+Simple example showing how to use the SoundClient provided by libsoundplay,
+in blocking, non-blocking, and explicit usage.
+"""
+import rospy
+from sound_play.libsoundplay import SoundClient
+from sound_play.srv import Talk
+
+class SoundClientRos:
+    def __init__(self):
+        s = rospy.Service('/bica_dialog/talk', Talk, self.talkCallback)
+    def talkCallback(self,req):
+        soundhandle = SoundClient(blocking=True)
+        soundhandle.say(req.str)
+        return []
+
+if __name__ == '__main__':
+    rospy.init_node('ros_soundclient', anonymous=False)
+    sound_client = SoundClientRos()
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
